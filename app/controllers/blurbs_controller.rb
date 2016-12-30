72f2a1700644555dc1dda376d128678310180a38
Dean <deangalvin3@gmail.com>
2016-12-29 22:42:28 -0500

    Remove swp & access to api clients

--- app/controllers/blurbs_controller.rb
+++ app/controllers/blurbs_controller.rb
@@ -1,11 +1,4 @@
 class BlurbsController < ApplicationController
-  before_action :set_blurb, only: [:show, :update, :destroy]
-
-  # GET /blurbs/1
-  def show
-    render json: @blurb
-  end
-
   # POST /blurbs
   def create
     @blurb = Blurb.new(blurb_params)
@@ -17,25 +10,7 @@ class BlurbsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /blurbs/1
-  def update
-    if @blurb.update(blurb_params)
-      render json: @blurb
-    else
-      render json: @blurb.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /blurbs/1
-  def destroy
-    @blurb.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blurb
-      @blurb = Blurb.find(params[:id])
-    end
 
     # Only allow a trusted parameter "white list" through.
     def blurb_params
