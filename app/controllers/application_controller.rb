3a352cdb56e623c92fa0dc5f4fafb2374af87113
Dean <deangalvin3@gmail.com>
2019-07-28 12:53:27 -0700

    remove authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_client
-
-  protected
-  def api_client
-    @api_client
-  end
-
-  private
-  def authenticate_client
-    @api_client = ApiClient.find(get_authorization)
-  end
-
-  def get_authorization
-    request.headers["HTTP_AUTHORIZATION"].gsub('JeffreyBotterhill ', '')
-  end
 end
