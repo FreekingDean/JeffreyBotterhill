19f831dd4dbf9472687789786165a7f49dd9fcd4
Dean <deangalvin3@gmail.com>
2019-07-28 12:45:31 -0700

    added slack controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :blurbs, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :slack do
+    resources :messages, only: [:create]
+    resources :mentions, only: [:create]
+  end
 end
