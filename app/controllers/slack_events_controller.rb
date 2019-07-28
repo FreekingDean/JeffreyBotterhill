1a38574aaebb9231b76da1f4bb084ba919e30685
Dean <deangalvin3@gmail.com>
2019-07-28 13:00:35 -0700

    learn again

--- app/controllers/slack_events_controller.rb
+++ app/controllers/slack_events_controller.rb
@@ -2,6 +2,13 @@ class SlackEventsController < ApplicationController
   before_action :handle_challenge
 
   def create
+    if params[:type] != 'event_callback'
+      error "Uh oh! #{params}"
+    end
+
+    if params[:event][:type] == 'message'
+      Learn.train_phrase(params[:event][:text])
+    end
   end
 
   private
