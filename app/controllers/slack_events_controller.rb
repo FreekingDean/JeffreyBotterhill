02f66339304aeb5c9c6f70ad512b9c52cd92ffa1
Dean <deangalvin3@gmail.com>
2019-07-28 12:56:36 -0700

    fix json response

--- app/controllers/slack_events_controller.rb
+++ app/controllers/slack_events_controller.rb
@@ -7,7 +7,7 @@ class SlackEventsController < ApplicationController
   private
   def handle_challenge
     if params[:type] == 'url_verification'
-      render :json, {challenge: params[:challenge]}
+      render json: {challenge: params[:challenge]}
     end
   end
 end
