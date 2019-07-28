08e1af9f24494252809ed8ac86fc185db7694ff3
Dean <deangalvin3@gmail.com>
2019-07-28 13:09:39 -0700

    Start responding

--- app/controllers/slack_events_controller.rb
+++ app/controllers/slack_events_controller.rb
@@ -2,12 +2,17 @@ class SlackEventsController < ApplicationController
   before_action :handle_challenge
 
   def create
-    if params[:type] != 'event_callback'
-      error "Uh oh! #{params}"
+    if params[:type] == 'app_mention'
+      query = Lexicon.clean(params[:text])
+      resp = Markov.go(query) 
+      info resp
     end
 
-    if params[:event][:type] == 'message'
-      Learn.train_phrase(params[:event][:text])
+    if params[:type] == 'event_callback'
+      if params[:event][:type] == 'message'
+        query = Lexicon.clean(params[:event][:text])
+        Learn.train_phrase(query)
+      end
     end
   end
 
